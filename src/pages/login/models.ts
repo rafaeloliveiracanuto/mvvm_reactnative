f7b8014e6a93f61d60a115756133f60530e5844a
Rafael Canuto <rafaeloliveiracanuto@gmail.com>
2022-10-04 00:01:09 -0300

    Start creating login models, viewModel and view and some repositories

---
+++ src/pages/login/models.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export interface LoginViewModel {
+  email: string
+  password: string
+  setPassword: React.Dispatch<React.SetStateAction<string>>
+  setEmail: React.Dispatch<React.SetStateAction<string>>
+  isLoading: boolean
+  onSubmit: () => void
+}
---
+++ src/pages/login/view.model.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Alert } from 'react-native'
+import { login } from '../../common/models/repositories/auth.repository'
+
+const useLoginViewModel = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const onSubmit = async () => {
+    console.log({ email, password })
+
+    try {
+      setIsLoading(true)
+      const response = await login({ email, password })
+      console.log(response)
+    } catch (error) { 
+      console.log(error)
+      Alert.alert('Oops!', 'Something went wrong!')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return {
+    email,
+    password,
+    setPassword,
+    setEmail,
+    isLoading,
+    onSubmit,
+  }
+}
+
+export default useLoginViewModel
